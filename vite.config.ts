1e809fa343491a92622447540bb1489e8c807809
sonsu95 <leesonsu07@gmail.com>
2025-07-10 17:50:15 +0900

    chore: integrate Tailwind CSS and update configuration for Prettier and Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import path from 'path';
 
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
 
 // Absolute path import configuration for Vite
 // https://vitejs.dev/config/shared-options.html#resolve-alias
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       '@': path.resolve(process.cwd(), './src'),
